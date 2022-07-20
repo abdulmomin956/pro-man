5dc79ce17a5a7bd68111a526a7c6ae2e08a052e7
Sayed <mdsayedgv@gmail.com>
2022-07-21 03:21:40 +0600

    Create Workspace nav in Board route

--- src/components/firstScreen/Board.js
+++ src/components/firstScreen/Board.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { FaBoxes } from "react-icons/fa";
+import { FaBoxes, FaUserFriends, FaCogs } from "react-icons/fa";
+import { HiViewGridAdd } from "react-icons/hi";
+import { GrUpgrade } from "react-icons/gr";
 
 const Board = () => {
   const popularTemplates = [
@@ -62,6 +64,7 @@ const Board = () => {
           <FaBoxes className="mr-3"></FaBoxes>Most popular templates
         </span>
       </h2>
+                {/* **Most popular template section start here** */}
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 ">
         {popularTemplates.map((template) => (
           <div className="align-middle" style={{ height: "100px", maxWidth: "180px", borderRadius: "5px", 
@@ -73,6 +76,34 @@ const Board = () => {
           </div>
         ))}
       </div>
+                {/* **Most popular template section End here** */}
+
+
+                {/* **Your Workspace section start here** */}
+      <div className="my-16">
+            <p className="text-2xl font-bold text-gray-500">YOUR WORKSPACES</p>
+        <div className="md:flex items-center justify-between">
+            <div className="flex items-center my-5">
+                <span className="bg-primary text-3xl font-bold rounded text-white px-2">Pm</span>
+                <h2 className="text-2xl font-bold ml-4">ProMan Workspace</h2>
+            </div>
+            <div className="grid grid-cols-3 lg:grid-cols-5 gap-1">
+                <div><button className="bg-gray-200 px-2 my-2 py-1 rounded-md hover:pointer-events-auto text-xl font-semibold hover:font-bold hover:bg-gray-300 flex items-center"><FaBoxes className="mr-1 text-sm"></FaBoxes> Boards</button> </div>
+                <div><button className="bg-gray-200 px-2 my-2 py-1 rounded-md hover:pointer-events-auto text-xl font-semibold hover:font-bold hover:bg-gray-300 flex items-center"><HiViewGridAdd className="mr-1 text-sm"></HiViewGridAdd> Views</button> </div>
+                <div><button className="bg-gray-200 px-2 my-2 py-1 rounded-md hover:pointer-events-auto text-xl font-semibold hover:font-bold hover:bg-gray-300 flex items-center"><FaUserFriends className="mr-1 text-sm"></FaUserFriends> Members</button> </div>
+                <div><button className="bg-gray-200 px-2 my-2 py-1 rounded-md hover:pointer-events-auto text-xl font-semibold hover:font-bold hover:bg-gray-300 flex items-center"><FaCogs className="mr-1 text-sm"></FaCogs> Settings</button> </div>
+                <div><button className="bg-gray-200 px-2 my-2 py-1 rounded-md hover:pointer-events-auto text-xl font-semibold hover:font-bold hover:bg-gray-300 flex items-center"><GrUpgrade className="mr-1 text-sm"></GrUpgrade> Upgrade</button> </div>
+               
+            </div>
+        </div>
+        <div>
+                Create New Board
+        </div>
+      </div>
+                {/* **Your Workspace section End here** */}
+
+
+
     </div>
   );
 };
