ff5365c2fecc898ea26a4d8a1cd3ed3477efb629
salmakhatun12345 <ummesalma0056@gmail.com>
2022-07-20 22:01:15 +0600

    navbar polising

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -18,9 +18,10 @@ const Navbar = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+
                         <li tabindex="0">
                             <a class="justify-between">
-                                Parent
+                                Workspaces
                                 <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg>
                             </a>
                             <ul class="p-2">
@@ -30,11 +31,11 @@ const Navbar = () => {
                         </li>
                         <li tabindex="0">
                             <button>
-                                Parent
+                                Create
 
                             </button>
                             <ul class="p-2">
-                                <li><a>Submenu 1</a></li>
+                                <li ><a >Item 1</a></li>
                                 <li><a>Submenu 2</a></li>
                             </ul>
                         </li>
@@ -45,23 +46,24 @@ const Navbar = () => {
                 <a href='/' class="btn btn-ghost normal-case text-xl">PRO-MAN</a>
                 <div class="navbar-center hidden lg:flex">
                     <ul class="menu menu-horizontal p-0">
-                        <li style={{ zIndex: '100' }} className='px-2' tabindex="0">
-                            <a>
-                                Workspaces
+
+                        <div class="dropdown">
+                            <label tabindex="0" class="btn btn-md m-1">Workspaces <a>
                                 <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
-                            </a>
-                            <ul class="p-2 bg-slate-100">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
+                            </a></label>
+                            <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+                                <li ><a>Item 1</a></li>
+                                <li><a>Item 2</a></li>
                             </ul>
-                        </li>
-                        <li tabindex="0">
-                            <button style={{ backgroundColor: '#BFC9FF' }} className='btn '> Create</button>
-                            <ul class="p-2 bg-slate-100">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
+                        </div>
+
+                        <div class="dropdown">
+                            <label tabindex="0" class="btn  btn-md m-1">Create</label>
+                            <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+                                <li ><a >Item 1</a></li>
+                                <li><a>Item 2</a></li>
                             </ul>
-                        </li>
+                        </div>
 
 
                     </ul>
