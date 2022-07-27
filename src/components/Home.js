77f29f9d27a3f2408af0248fdd33d7ddccd9bb11
Abdul Momin <66585914+abdulmomin956@users.noreply.github.com>
2022-07-27 18:50:52 +0600

    homescreen completed

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,11 +4,10 @@ import Sidebar from './Sidebar/Sidebar';
 
 const Home = () => {
     return (
-        <div style={{ backgroundColor: "#F5F5F5", zIndex: '0' }}>
+        <div className='px-11' style={{ backgroundColor: "#F5F5F5", zIndex: '0' }}>
             {/* <Navbar></Navbar> */}
             {/* <h1>This is Home</h1> */}
             <Sidebar></Sidebar>
-            <h2>home </h2>
         </div>
     );
 };
--- src/components/firstScreen/HomeScreen.js
+++ src/components/firstScreen/HomeScreen.js
@@ -1,10 +1,26 @@
 import React from 'react';
-import CoomingSoon from '../shared/CoomingSoon';
+import { FaRegPlusSquare } from 'react-icons/fa';
+import img from '../../images/uptodate.png'
 
 const HomeScreen = () => {
     return (
-        <div>
-            <CoomingSoon></CoomingSoon>
+        <div className='flex justify-start mx-10'>
+            <div class="card w-[39rem] bg-base-100 shadow-xl">
+                <figure><img src={img} alt="Shoes" /></figure>
+                <div class="card-body text-center">
+                    <h1 class="text-2xl text-center">Stay on track and up to date</h1>
+                    <p>Invite people to boards and cards, leave comments, add due dates, and we'll show the most important activity here.</p>
+
+                </div>
+            </div>
+            <div className='ml-10'>
+                <p>Links</p>
+                <div className='btn w-80 justify-start'>
+                    <FaRegPlusSquare></FaRegPlusSquare>
+                    <span className='ml-3'>Create a board</span>
+                </div>
+            </div>
+
         </div>
     );
 };
