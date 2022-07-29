54d458e9e903e1dae7e2ea4d7c78c4e366a8e057
salmakhatun12345 <ummesalma0056@gmail.com>
2022-07-29 17:35:04 +0600

    recheck

--- src/components/boardDetails/BoardDetails.js
+++ src/components/boardDetails/BoardDetails.js
@@ -109,7 +109,6 @@ const BoardDetails = () => {
     return (
 
         <storeApi.Provider value={{ addMoreCard, addMoreList }}>
-            <h2>Salma Salma Salma</h2>
             <DragDropContext onDragEnd={onDragEnd}>
                 <Droppable droppableId='boardDetails' type='list' direction='horizontal'>
                     {(provided) => (
