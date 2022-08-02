798b37d6311c5181acd9f2caafc98fa2e7c11308
ssmdbayzid <ssmd.bayzid@gmail.com>
2022-08-03 01:47:34 +0600

    Create Dynamic BG for Board & Board Details

--- src/components/Workspace/Workspace.js
+++ src/components/Workspace/Workspace.js
@@ -164,9 +164,9 @@ const Workspace = () => {
                   </label>
                 </div>
 
-                <div className="flex justify-center ">
+                <div className="flex justify-center my-5">
                   {workspaceName ? (
-                    <div className="w-2/3">
+                    <div className="w-4/5">
                       <button
                         type="submit"
                         className="w-full cursor-pointer p-2 pl-5 pr-5 transition-colors duration-700 transform bg-indigo-500 hover:bg-blue-400 text-gray-100 text-lg rounded-lg focus:border-4 border-indigo-300"
@@ -180,7 +180,7 @@ const Workspace = () => {
                       </button>
                     </div>
                   ) : (
-                    <div className="w-2/3">
+                    <div className="w-4/5">
                       <button
                         type="submit"
                         className="w-full p-2 pl-5 pr-5 transition-colors duration-700 transform bg-indigo-500 hover:bg-blue-400 text-gray-100 text-lg rounded-lg focus:border-4 border-indigo-300"
