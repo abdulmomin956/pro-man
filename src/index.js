b1edb8808a11318a7f430ee6bca24dc28d03ba30
Abdul Momin <66585914+abdulmomin956@users.noreply.github.com>
2022-08-03 16:51:29 +0600

    two api connected

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
@@ -13,11 +13,11 @@ root.render(
 
   // <React.StrictMode>
 
- <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <App />
     </BrowserRouter>
+  </QueryClientProvider>
   // </React.StrictMode>
 
 );
