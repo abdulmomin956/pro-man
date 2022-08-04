aeb8efdf1104772a8c523358ad95eb6adc556cfb
Abdul Momin <66585914+abdulmomin956@users.noreply.github.com>
2022-08-04 14:45:48 +0600

    redux setup

---
+++ src/global-state/constants/reduxContstants.js
@@ -0,0 +1,2 @@
+export const LAST_WORKSPACE = "LAST_WORKSPACE"
+export const WORKSPACE = "WORKSPACE"
--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Provider } from "react-redux";
+import store from "./store";
+
+
 
 const queryClient = new QueryClient();
 
@@ -15,7 +19,9 @@ root.render(
 
   <QueryClientProvider client={queryClient}>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </QueryClientProvider>
   // </React.StrictMode>
