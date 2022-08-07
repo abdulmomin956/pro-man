f750e9ca987b3c2f5a1961811fd49603a4a55a29
Sayed <mdsayedgv@gmail.com>
2022-08-08 02:48:45 +0600

    Workspace create button add in Sidebar.js

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -2,9 +2,10 @@ import React from "react";
 import { FaArrowAltCircleRight } from "react-icons/fa";
 import { NavLink, Outlet } from "react-router-dom";
 import "./Sidebar.css";
+import { Link } from "react-router-dom";
+import { MdAddCircleOutline } from "react-icons/md";
 
 const Sidebar = () => {
-
   return (
     <div>
       <div className="drawer drawer-mobile static">
@@ -19,10 +20,7 @@ const Sidebar = () => {
             <FaArrowAltCircleRight className="text-3xl"></FaArrowAltCircleRight>
           </label>
         </div>
-        <div
-          className="drawer-side lg:mx-7 rounded shadow"
-
-        >
+        <div className="drawer-side lg:mx-7 rounded shadow">
           <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
           <ul className="menu p-4 overflow-y-auto w-52 bg-base-100 text-base-content">
             {/* <!-- Sidebar content here --> */}
@@ -41,6 +39,13 @@ const Sidebar = () => {
                 Home
               </NavLink>
             </li>
+
+            <div className="flex items-center justify-between bg-none mt-5">
+              <p>Workspace</p>
+              <label htmlFor="my-modal-sa6" className="myButton p-1 rounded-sm">
+                <MdAddCircleOutline className="text-xl  "></MdAddCircleOutline>
+              </label>
+            </div>
           </ul>
         </div>
       </div>
