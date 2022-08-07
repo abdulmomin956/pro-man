6571b8c25b54f133f2980d9f1804ae09f29240d9
Abdul Momin <66585914+abdulmomin956@users.noreply.github.com>
2022-08-07 23:24:20 +0600

    create new route

---
+++ src/components/Workspace/Boards.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Boards = () => {
+    return (
+        <div>
+            this is board setting page
+        </div>
+    );
+};
+
+export default Boards;
---
+++ src/components/Workspace/Members.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Members = () => {
+    return (
+        <div>
+            This is members setting page
+        </div>
+    );
+};
+
+export default Members;
