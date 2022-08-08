91945bcdf955208caeea85f20a8777c18c321f77
Abdul Momin <66585914+abdulmomin956@users.noreply.github.com>
2022-08-08 13:08:51 +0600

    workpace page simple update

--- src/components/Workspace/Boards.js
+++ src/components/Workspace/Boards.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useParams } from 'react-router-dom'
 
 const Boards = () => {
+    const { workspaceID } = useParams()
+    console.log(workspaceID);
     return (
         <div>
             this is board setting page
