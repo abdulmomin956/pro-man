d6183dd26f56197fac4acde701fa2643b57f9edc
muzahid37 <muzahidul372709@gmail.com>
2022-08-09 01:58:11 +0600

    add nested route and active route on profie page

---
+++ src/components/Profile/ProfileSetting.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CoomingSoon from "../shared/CoomingSoon";
+
+const ProfileSetting = () => {
+    return (
+        <div>
+            <CoomingSoon></CoomingSoon>
+        </div>
+    );
+};
+
+export default ProfileSetting;
