16c71a2dfd7e20ed49218fb517d0da717d1c2128
salmakhatun12345 <ummesalma0056@gmail.com>
2022-08-24 06:06:43 +0600

    warning handeling

--- src/utils/store.js
+++ src/utils/store.js
@@ -1,17 +1,4 @@
-const cards = [
-    // {
-    //     id: 'card-1',
-    //     title: 'Learning how to cook',
-    // },
-    // {
-    //     id: 'card-2',
-    //     title: 'Making sandwich',
-    // },
-    // {
-    //     id: 'card-3',
-    //     title: 'Taking the trash out',
-    // },
-];
+
 
 const data = {
     lists: {
