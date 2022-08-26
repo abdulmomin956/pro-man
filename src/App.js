6d87747596c3f32fba75e3e1d450a8eee6dcf13c
Mahadeb Sen <mahadebsen237@gmail.com>
2022-08-26 09:13:30 +0530

    images replaced

--- src/App.js
+++ src/App.js
@@ -28,17 +28,15 @@ import Guests from "./components/Workspace/Member/Guests";
 import Pending from "./components/Workspace/Member/Pending";
 import { useSelector } from "react-redux";
 import MakeAdmin from "./components/dashboard/MakeAdmin";
-
+import TemplateCategory from "./components/firstScreen/TemplateComponents/TemplateCategory";
 
 function App() {
-  const email = useSelector(state => state.email)
+  const email = useSelector((state) => state.email);
 
   return (
     <div className="#F5F5F5">
-
       {/* <ToastContainer /> */}
       {email && <Navbar />}
-
 
       <Routes>
         <Route
@@ -51,15 +49,21 @@ function App() {
         >
           <Route path="/" element={<Board />}></Route>
           <Route path="/template" element={<Template />}></Route>
+          <Route
+            path="/template/:category"
+            element={<TemplateCategory></TemplateCategory>}
+          ></Route>
           <Route path="/homescreen" element={<HomeScreen />}></Route>
         </Route>
-        <Route path="/makeadmin" element={<MakeAdmin/>}></Route>
-        <Route path="/:shortname" element={
-          <RequireAuth>
-            <Workspace />
-          </RequireAuth>
-        }>
-
+        <Route path="/makeadmin" element={<MakeAdmin />}></Route>
+        <Route
+          path="/:shortname"
+          element={
+            <RequireAuth>
+              <Workspace />
+            </RequireAuth>
+          }
+        >
           <Route path="/:shortname/" element={<Boards />}></Route>
           <Route path="/:shortname/members" element={<Members />}>
             <Route
