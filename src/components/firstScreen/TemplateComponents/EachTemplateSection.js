cb585dfdbd5cdb315c4b5e93e08a22d44135bd28
Mahadeb Sen <mahadebsen237@gmail.com>
2022-08-26 10:37:37 +0530

    lode templates added

--- src/components/firstScreen/TemplateComponents/EachTemplateSection.js
+++ src/components/firstScreen/TemplateComponents/EachTemplateSection.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { AllTemplates } from "../../Home";
 import EachTemplate from "./EachTemplate";
 
-const EachTemplateSection = () => {
+const EachTemplateSection = ({ params }) => {
+  const [templates, setTemplates] = useState([]);
+  const { popularTemplates } = useContext(AllTemplates);
+
+  useEffect(() => {
+    const templates = popularTemplates.filter(
+      (item) => item.categorie === { params }
+    );
+    setTemplates(templates);
+  }, [params, popularTemplates]);
+
   return (
     <div>
       <div className="flex items-center flex-row gap-3 mb-6">
@@ -10,12 +23,12 @@ const EachTemplateSection = () => {
           src="https://a.trellocdn.com/prgb/dist/images/templates/categories/small/design.e617072931480fdd44da.svg"
           alt=""
         />
-        <p className="text-xl font-semibold">Design</p>
+        <p className="text-xl font-semibold">{params}</p>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-        {/* {designTemplates.map((item) => (
-            <EachTemplate key={item._id} item={item}></EachTemplate>
-          ))} */}
+        {templates.map((item) => (
+          <EachTemplate key={item._id} item={item}></EachTemplate>
+        ))}
       </div>
     </div>
   );
--- src/components/firstScreen/TemplateComponents/TemplateCategory.js
+++ src/components/firstScreen/TemplateComponents/TemplateCategory.js
@@ -1,12 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useContext } from "react";
 import { useParams } from "react-router-dom";
+import { AllTemplates } from "../../Home";
+import EachTemplate from "./EachTemplate";
+import TemplateCreateSection from "./TemplateCreateSection";
 
 const TemplateCategory = () => {
   const params = useParams();
-  console.log(params);
+  const category = params.category;
+  const { popularTemplates } = useContext(AllTemplates);
+  const [templates, setTemplates] = useState([]);
+
+  useEffect(() => {
+    const filterOutTemplates = popularTemplates.filter(
+      (item) => item.categorie === "Engineering"
+    );
+    setTemplates(filterOutTemplates);
+  }, [popularTemplates, category]);
+
+  console.log(popularTemplates);
+  console.log(templates);
+
   return (
     <div>
-      <p>hiii from {params.category}</p>
+      <div>
+        <div className="flex items-center flex-row gap-3 mb-6">
+          <img
+            className="w-[24px] rounded-md"
+            src="https://a.trellocdn.com/prgb/dist/images/templates/categories/small/design.e617072931480fdd44da.svg"
+            alt=""
+          />
+          <p className="text-xl font-semibold">{category}</p>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+          {templates.map((item) => (
+            <EachTemplate key={item._id} item={item}></EachTemplate>
+          ))}
+        </div>
+      </div>
+      <TemplateCreateSection></TemplateCreateSection>
     </div>
   );
 };
