8060ed4454d77e93260ca42134296196d6c028b8
Mahadeb Sen <mahadebsen237@gmail.com>
2022-09-01 21:24:37 +0530

    changes

--- src/components/firstScreen/Template.js
+++ src/components/firstScreen/Template.js
@@ -64,7 +64,6 @@ const Template = () => {
   }, [popularTemplates]);
 
   const handleLoadMoreTemplates = (category) => {
-    // console.log(category);
     navigate("/template/" + category);
   };
 
